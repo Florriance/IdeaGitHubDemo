d118f2df0ac3859badf88ecf3fadc68bc19ff3aa
Florriance <970847592@qq.com>
2020-04-11 20:23:19 +0800

    Second commit

--- src/com/my/package00/GitHubDemo00.java
+++ src/com/my/package00/GitHubDemo00.java
@@ -2,6 +2,6 @@ package com.my.package00;
 
 public class GitHubDemo00 {
     public static void main(String[] args){
-        System.out.println("GitHub Test Version3.0");
+        System.out.println("GitHub Test Version2.0");
     }
 }
